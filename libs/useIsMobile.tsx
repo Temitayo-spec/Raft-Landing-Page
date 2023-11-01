c5f95d7adf4579568316eb6d6282d6322465b611
Temmy <olawanletemitayo@gmail.com>
2023-11-02 00:37:09 +0100

    Update useIsMobile.tsx

--- libs/useIsMobile.tsx
+++ libs/useIsMobile.tsx
@@ -6,7 +6,7 @@ const isClient = typeof window === 'object';
 
 export const useIsMobile = () => {
   const pathname = usePathname();
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const [isMobile, setIsMobile] = useState(isClient && window.innerWidth <= 768);
 
   useEffect(() => {
     const handleResize = () => {
